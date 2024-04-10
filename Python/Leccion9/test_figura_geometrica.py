654d35b99b282039fe21e2da3a98533c1b43c88b
Franco <francopaganoo2015@gmail.com>
2024-04-10 00:56:20 -0300

    Método MRO: Method Resolution Order

--- Python/Leccion9/test_figura_geometrica.py
+++ Python/Leccion9/test_figura_geometrica.py
@@ -4,3 +4,6 @@ cuadrado1 = Cuadrado(7, "verde")
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f"Calculo del area del cuadrado: {cuadrado1.calcular_area()}")
+
+# MRO = Method Resolution Order
+print(Cuadrado.mro())
