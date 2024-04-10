b10199198f33f56b5d81be1a673b45f3fa67b1c4
Franco <francopaganoo2015@gmail.com>
2024-04-10 00:26:08 -0300

    Creamos la clase hija Cuadrado

---
+++ Python/Leccion9/Cuadrado.py
@@ -0,0 +1,11 @@
+from Color import Color
+from FiguraGeometrica import FiguraGeometrica
+
+class Cuadrado(FiguraGeometrica, Color):
+    def __int__(self, lado, color):
+        #super.__init__(lado)
+        FiguraGeometrica.__int__(self,lado,lado)
+        Color.__int__(self,color)
+
+    def calcular_area(self):
+        return self.alto * self.ancho
