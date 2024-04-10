9673101dca4f6bc903f79d141b79e895db8eb9c3
Franco <francopaganoo2015@gmail.com>
2024-04-10 20:55:59 -0300

    Palabra reservada Continue

--- JavaScript/TSLeccion01/01-03-ciclos.js
+++ JavaScript/TSLeccion01/01-03-ciclos.js
@@ -29,3 +29,11 @@ for(let contando = 0; contando < 10; contando++){
 }
 console.log("Termina el ciclo al encontrar el primer numero los pares");
 
+// la palabra continue
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        continue; // ir a la siguiente iteracion
+    }
+    console.log(contando); //Muestra todos los pares
+}
+console.log("Termina el ciclo");
