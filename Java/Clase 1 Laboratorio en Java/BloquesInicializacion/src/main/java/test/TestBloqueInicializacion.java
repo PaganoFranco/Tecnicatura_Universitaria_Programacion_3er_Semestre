5abf5899c967f3d62137fd00aa2eeacc0f09cc5a
Franco <francopaganoo2015@gmail.com>
2024-04-12 21:38:41 -0300

    Cierre Clase 2 Bloques y mucho más

--- Java/Clase 1 Laboratorio en Java/BloquesInicializacion/src/main/java/test/TestBloqueInicializacion.java
+++ Java/Clase 1 Laboratorio en Java/BloquesInicializacion/src/main/java/test/TestBloqueInicializacion.java
@@ -8,5 +8,7 @@ public class TestBloqueInicializacion {
     public static void main(String[] args) {
         Persona persona1 = new Persona();
         System.out.println("persona1 = " + persona1);
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
     }
 }
