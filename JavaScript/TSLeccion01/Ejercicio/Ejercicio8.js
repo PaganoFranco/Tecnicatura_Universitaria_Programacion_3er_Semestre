f90d985c78defb4b37c9350c80679b31063aa36c
Franco <francopaganoo2015@gmail.com>
2024-04-15 21:09:38 -0300

    Ejercicios ciclos JavaScript

---
+++ JavaScript/TSLeccion01/Ejercicio/Ejercicio8.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 8: Pedir un numero N, y mostrar todos los numeros del 1 al N.
+ */
+
+let numero = parseInt(prompt("Ingrese un numero: "));
+alert("Los numeros son: ");
+for(let i = 1; i <= numero; i++){
+    alert(i)
+}
