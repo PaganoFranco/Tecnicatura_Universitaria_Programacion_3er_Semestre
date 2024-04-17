4ebd6a3c1607783231ae2e899c99cc5cd529b9ec
Franco <francopaganoo2015@gmail.com>
2024-04-17 09:17:12 -0300

    Explicación de validaciones setter

--- Python/Leccion9/FiguraGeometrica.py
+++ Python/Leccion9/FiguraGeometrica.py
@@ -22,7 +22,8 @@ class FiguraGeometrica:
     def ancho(self, ancho):
         if self._validar_valores(ancho):
             self._ancho = ancho
-
+        else:
+            print(f"valor arroneo ancho: {ancho}")
     # Getter para obtener el valor del alto
     @property
     def alto(self):
@@ -32,8 +33,9 @@ class FiguraGeometrica:
     @alto.setter
     def alto(self, alto):
         if self._validar_valores(alto):
-            self.alto = alto
-
+            self._alto = alto
+        else:
+            print(f"valor arroneo ancho: {alto}")
     # Método ToString
     def __str__(self):
         return f"FiguraGeometrica [Ancho: {self._ancho}, Alto: {self.alto}]"
--- Python/Leccion9/test_figura_geometrica.py
+++ Python/Leccion9/test_figura_geometrica.py
@@ -2,15 +2,14 @@ from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 
 print("Creacion de objeto clase Cuadradro".center(50, "_"))
-cuadrado1 = Cuadrado(5, "Azul")
-cuadrado1.alto = -10
-print("Cuadrado 1")
-print(f"Ancho: {cuadrado1.ancho}")
-print(f"Alto: {cuadrado1.alto}")
+cuadrado1 = Cuadrado(8, "Azul")
+cuadrado1.alto = 7
+# print(f"Ancho: {cuadrado1.ancho}")
+#  print(f"Alto: {cuadrado1.alto}")
 print(f"Calculo del area del cuadrado: {cuadrado1.calcular_area()}")
 
 # MRO = Method Resolution Order
-print(Cuadrado.mro())
+# print(Cuadrado.mro())
 
 print(cuadrado1)
 
@@ -21,10 +20,10 @@ print(f"Alto: {cuadrado2.alto}")
 print(f"Calculo del area del cuadrado: {cuadrado2.calcular_area()}")
 print(cuadrado1)
 
-rectangulo1 = Rectangulo(3, 2, "Rojo")
+print("Creacion de objeto clase Rectangulo".center(50, "_"))
+rectangulo1 = Rectangulo(3, 9, "Rojo")
 rectangulo1.ancho = 15
-print("Rectangulo 1")
-print(f"Ancho: {rectangulo1.ancho}")
-print(f"Alto: {rectangulo1.alto}")
+# print(f"Ancho: {rectangulo1.ancho}")
+# print(f"Alto: {rectangulo1.alto}")
 print(f"Calculo del area del rectangulo: {rectangulo1.calcular_area()}")
 print(rectangulo1)
