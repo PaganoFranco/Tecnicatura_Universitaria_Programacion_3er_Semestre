c202dd5ac5d1b24263951064a97dd3ffcd4b6bca
Franco <francopaganoo2015@gmail.com>
2024-04-20 00:09:18 -0300

    Constantes

---
+++ Python/Leccion10/Constantes.py
@@ -0,0 +1,12 @@
+# El concepto de contante consta de definir una variable cuyo valor no podemos modificar, pero
+# en python las constantes se denominan convenciones, el mismo no lo acepta como constante,
+# pero si puede simular el concepto.
+
+# Constantes en Python:
+# 1_ Debe definirse el nombre de la variable en mayuscula.
+
+class Matematicas:
+    PI = 3.1614 # Esta es una variable de clase constante
+
+MI_CONSTATE = "Esta es una variable constante"
+
---
+++ Python/Leccion10/test_constantes.py
@@ -0,0 +1,8 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTATE)
+# MI_CONSTATE = "Aquí esta el nuevo valor para la constate"
+# print(MI_CONSTATE)
+
+print(Mate)
