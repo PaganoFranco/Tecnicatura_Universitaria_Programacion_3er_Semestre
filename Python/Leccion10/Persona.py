31b6286b07bc7d09d2f038bb6c430ef941e307a2
Franco <francopaganoo2015@gmail.com>
2024-04-22 09:15:27 -0300

    Mejoras en el ejercicio Contador

--- Python/Leccion10/Persona.py
+++ Python/Leccion10/Persona.py
@@ -1,9 +1,14 @@
 class Persona:
     contador_personas = 0  # Variable de clase
 
+    # Agregamos un metodo de clase
+    @classmethod
+    def generar_siguiente_valor(cls):
+        cls.contador_personas += 1  # Vamos inrementando
+        return cls.contador_personas
+
     def __init__(self, nombre, edad):
-        Persona.contador_personas += 1  # vamos incrementando
-        self.id_persona = Persona.contador_personas
+        self.id_persona = Persona.generar_siguiente_valor()
         self.nombre = nombre
         self.edad = edad
 
@@ -17,5 +22,9 @@ persona2 = Persona("Juli", 27)
 print(persona2)
 persona3 = Persona("Juan Pablo", 27)
 print(persona3)
+Persona.generar_siguiente_valor()
+persona4 = Persona("Agos", 25)
+print(persona4)
 
 print(f"Valor contador persona: {Persona.contador_personas}")
+
