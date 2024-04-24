a6834abf5c9ad90aa0bbe820d009ffe05ce1ab2e
Franco <francopaganoo2015@gmail.com>
2024-04-24 00:07:40 -0300

    Recorremos los elementos de un arreglo

--- JavaScript/02-03-arreglos.js
+++ JavaScript/02-03-arreglos.js
@@ -4,5 +4,12 @@
 // let autos = new Array("Ferrari", "Renaul", "BMW"); SINTAXIS VIEJA 
 
 const autos = ["Ferrari", "Renaul", "BMW"];
-
 console.log(autos);
+
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i++){
+    console.log(i+": "+autos[i])
+}
