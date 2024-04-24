17cd2afba3b4e3dc0b3f622b92ae4373052055df
Franco <francopaganoo2015@gmail.com>
2024-04-24 00:18:26 -0300

    Modificamos los elementos

--- JavaScript/02-03-arreglos.js
+++ JavaScript/02-03-arreglos.js
@@ -13,3 +13,20 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
     console.log(i+": "+autos[i])
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = "Volvo";
+console.log(autos[1]);
+
+// Agregar nuevos valores al arreglo
+autos.push("Audi"); // Este nuevo elementos se agrega al final
+console.log(autos);
+
+// Otra forma de agregar elementos
+autos[autos.length] = "Proche";
+console.log(autos);
+
+//Tercera forma de agreghar elementos teniendo CUIDADO
+autos[6] = "Renault";
+console.log(autos);
+
