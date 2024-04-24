b0076aa12e8390174510fbd700be146c521f032b
Franco <francopaganoo2015@gmail.com>
2024-04-23 21:51:38 -0300

    Arreglos en JavaScript

---
+++ JavaScript/02-03-arreglos.js
@@ -0,0 +1,8 @@
+// Un arreglo es un tipo objeto
+// Formas de declarar un arreglo:
+
+// let autos = new Array("Ferrari", "Renaul", "BMW"); SINTAXIS VIEJA 
+
+const autos = ["Ferrari", "Renaul", "BMW"];
+
+console.log(autos);
