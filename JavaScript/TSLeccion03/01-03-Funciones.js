cee851a61f070a66367f831f3083c137e9556898
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-04-30 20:54:24 -0300

    Palabra return

--- JavaScript/TSLeccion03/01-03-Funciones.js
+++ JavaScript/TSLeccion03/01-03-Funciones.js
@@ -2,9 +2,12 @@ miFuncion(8, 2); //Podemos llamar a la funcion en cualquier momento, este antes
 // o despues de las misma "HOSTING" 
 
 function miFuncion(a, b){
-    console.log("suma: " + (a+b));
+    //console.log("suma: " + (a+b));
+    return a + b;
 }
 
 //Llamamos a la funcion
 miFuncion(5, 4);
 
+let resultado = miFuncion(6, 7);
+console.log(resultado);
