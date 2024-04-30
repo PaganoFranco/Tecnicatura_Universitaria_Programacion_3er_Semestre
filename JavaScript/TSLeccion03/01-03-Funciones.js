b3fbb5c7dd1abffa08b0f3523662474b9ac724be
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-04-30 20:47:21 -0300

    Introducción a Funciones

---
+++ JavaScript/TSLeccion03/01-03-Funciones.js
@@ -0,0 +1,10 @@
+miFuncion(8, 2); //Podemos llamar a la funcion en cualquier momento, este antes
+// o despues de las misma "HOSTING" 
+
+function miFuncion(a, b){
+    console.log("suma: " + (a+b));
+}
+
+//Llamamos a la funcion
+miFuncion(5, 4);
+
