44df3e8526cf171777a4057cb1707cb4b67c590a
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-01 20:44:54 -0300

    Polimorfismo

--- Java/New Folder/Sobreescritura/src/main/java/test/TesteSobreescritura.java
+++ Java/New Folder/Sobreescritura/src/main/java/test/TesteSobreescritura.java
@@ -1,12 +1,22 @@
 
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 public class TesteSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Franco", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
-        
+       Empleado empleado1 = new Empleado("Juan", 1000);
+       imprimir(empleado1);
+       // System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+
+       Gerente gerente1 = new Gerente("Franco", 5000, "Sistemas");
+       // System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+       imprimir(gerente1);
     }
+    
+    public static void imprimir(Empleado empleado) {
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+    
+    
 }
