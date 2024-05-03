0a2ef030ce1d6afe59cb1de3336a86cbf9469c01
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-03 10:16:19 -0300

    Agregamos métodos a los Objetos

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -6,11 +6,16 @@ let persona = {
     nombre: "Carlos",
     apellido: "Gil",
     email: "cgil@gmail.com",
-    edad: 30
+    edad: 30,
+    nombreCompleto: function(){ //método o funcion en JavaScript
+        return this.nombre + " "+ this.apellido
+    }
 }
 
 console.log(persona.nombre);
 console.log(persona.apellido);
 console.log(persona.email);
 console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto());
 
