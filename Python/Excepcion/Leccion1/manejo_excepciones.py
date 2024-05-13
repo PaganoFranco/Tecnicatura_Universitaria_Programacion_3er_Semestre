ff741fbc9b2fa4aab64f4b0cdb5e21215de09781
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-13 09:15:52 -0300

    Procesamiento de excepciones

--- Python/Excepcion/Leccion1/manejo_excepciones.py
+++ Python/Excepcion/Leccion1/manejo_excepciones.py
@@ -1,8 +1,15 @@
+resultado = None
+a = "10"
+b = 0
+
 try:
-    10 / 0
+    resultado = a / b # Modificamos
 # Mas generico
 except Exception as e:
     print(f"Ocurrio un error: {e}")
-# Mas espesifico
+# Mas especifico
 # except ZeroDivisionError as e:
 # print(f"Ocurrio un error: {e}")
+
+print(f"El resultado es: {resultado}")
+print("seguimos...")
