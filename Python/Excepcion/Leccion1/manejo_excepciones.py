4f3a0f3ad2cddc44c463dc540e3c8ecad53bfbbd
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-13 09:22:01 -0300

    Procesar clases de exception más específicas

--- Python/Excepcion/Leccion1/manejo_excepciones.py
+++ Python/Excepcion/Leccion1/manejo_excepciones.py
@@ -1,15 +1,17 @@
 resultado = None
-a = "10"
-b = 0
+a = 7
+b = 5
 
 try:
     resultado = a / b # Modificamos
-# Mas generico
+# Mas especifico - Clases Hijas
+except TypeError as e:
+    print(f"TypeError - Ocurrio un error: {type(e)}")
+except ZeroDivisionError as e:
+    print(f"ZeroDivisionError - Ocurrio un error: {type(e)}")
+# Mas generico - Clase Padre
 except Exception as e:
-    print(f"Ocurrio un error: {e}")
-# Mas especifico
-# except ZeroDivisionError as e:
-# print(f"Ocurrio un error: {e}")
+    print(f"Exception - Ocurrio un error: {type(e)}")
 
 print(f"El resultado es: {resultado}")
 print("seguimos...")
