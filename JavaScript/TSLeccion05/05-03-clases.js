027874233403559a657164d023403df44691d664
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-14 11:48:36 -0300

    Sintaxis de Clases en JavaScript

---
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -0,0 +1,13 @@
+class Persona{ 
+    //Creamos un constructor
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//Creamos objetos
+let persona1 = new Persona("Franco", "Pagano");
+console.log(persona1);
+let persona2 = new Persona("Julieta", "Quiroga");
+console.log(persona2);
