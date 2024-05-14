defc7cd6e68d5faa0cfeb14a8b3173ee5abf3627
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-14 11:58:12 -0300

    Método Get y Set: Parte Get y Parte Set

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,13 +1,42 @@
 class Persona{ 
     //Creamos un constructor
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    //Creamos el metodo get
+    get nombre(){
+        return this._nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    //Creamos el metodo set
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
     }
 }
 
 //Creamos objetos
 let persona1 = new Persona("Franco", "Pagano");
-console.log(persona1);
+//Visualizamos el nombre por medio del metodo get
+console.log(persona1.nombre);
+//Modificamos el nombre por medio del metodo set
+persona1.nombre = "Oscar";
+console.log(persona1.nombre);
+//console.log(persona1);
+
 let persona2 = new Persona("Julieta", "Quiroga");
-console.log(persona2);
+//Visualizamos el apellido por medio del metodo set
+console.log(persona2.apellido);
+//Modificamos el apellido por medio del metodo set
+persona2.apellido = "Pagano";
+console.log(persona2.apellido);
+//console.log(persona2);
