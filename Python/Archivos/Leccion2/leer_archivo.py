285285ba3757aca8528995e35d1caba47a4a5cb4
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-05-14 10:49:53 -0300

    Lectura de archivos

---
+++ Python/Archivos/Leccion2/leer_archivo.py
@@ -0,0 +1,8 @@
+
+archivo = open("C:\\Users\Franc\Tecnicatura_Universitaria_Programacion_3er_Semestre\Python\Archivos\Leccion2\prueba.txt", "r",
+               encoding="utf8") # Las letras son: "r" read, "a" append, "w" write, "x"
+# print(archivo.read())
+# print(archivo.read(15))
+# print(archivo.read(5)) # Continua desde la linea anterior
+print(archivo.readline())
+print(archivo.readline())
--- Python/Archivos/Leccion2/manejo_archivos.py
+++ Python/Archivos/Leccion2/manejo_archivos.py
@@ -4,6 +4,8 @@ try:
     archivo.write("Programamos con diferentes tipos de archivos, ahora en txt. \n")
     archivo.write("Los acentos son importantes para las palabras \n")
     archivo.write("como por ejemplo: ación, ejecución y producción \n")
+    archivo.write("Las letras son: \nr read leer, \na append anexa, \nw write escribir, \nx crear un archivo")
+    archivo.write("\nt esta es para texto o text, \nb archivo binarios, \nw+ leer y escribir son iguales r+ \n")
     archivo.write("Con esto terminamos")
 except Exception as e:
     print(e)
