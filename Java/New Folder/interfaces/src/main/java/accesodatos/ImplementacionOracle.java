88506a2e1ee244cb1e8bc682bc48d620899fd072
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-09 20:32:53 -0300

    Pruebas

--- Java/New Folder/interfaces/src/main/java/accesodatos/ImplementacionOracle.java
+++ Java/New Folder/interfaces/src/main/java/accesodatos/ImplementacionOracle.java
@@ -2,7 +2,7 @@
 package accesodatos;
 
 
-public class ImplementaconOracle implements IAccesoDatos{
+public class ImplementacionOracle implements IAccesoDatos{
 
     @Override
     public void insertar() {
