392b829c8dc188017ccaad3ea193efeec1e7604a
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-09 20:15:42 -0300

    Manejo de Interfaces Parte 4

--- Java/New Folder/interfaces/src/main/java/accesodatos/ImplementacionMySql.java
+++ Java/New Folder/interfaces/src/main/java/accesodatos/ImplementacionMySql.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package accesodatos;
 
-/**
- *
- * @author Franc
- */
-public class ImplementacionMySql {
+
+public class ImplementacionMySql implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("listar desde MySql");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("actualizar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("eliminar desde MySql");
+    }
     
 }
