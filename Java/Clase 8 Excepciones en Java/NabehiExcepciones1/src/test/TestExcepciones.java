781c96ef9a94bdf3f39bab90aecb626acd26d7f4
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-28 01:08:11 -0300

    Comenzamos a hacer pruebas con RuntimeException (unchecked)

--- Java/Clase 8 Excepciones en Java/NabehiExcepciones1/src/test/TestExcepciones.java
+++ Java/Clase 8 Excepciones en Java/NabehiExcepciones1/src/test/TestExcepciones.java
@@ -1,17 +1,18 @@
 
 package test;
 
-import aritmetica.Artimetica;
+import static aritmetica.Artimetica.division;
 
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-        try{
-            resultado = Artimetica.division(10, 0);
-        } catch(Exception e){
-            System.out.println("Ocurrio un Error");
-            e.printStackTrace(System.out); // Se conoce como la pila de excepciones
-        }
+        //try{
+            resultado = division(10, 0);
+        //} catch(Exception e){
+        //    System.out.println("Ocurrio un Error");
+        //    e.printStackTrace(System.out); // Se conoce como la pila de excepciones
+        //    System.out.println("");
+        //}
         System.out.println("La vairable de resultado tiene como valor "+resultado);
     }
 }
