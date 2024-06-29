7ba44f34f0ffccd28223ea60ab907ed25d738a8b
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-29 20:06:22 -0300

    Uso de with y psycopg2

--- Python/Leccion04/BD/prueba_bd.py
+++ Python/Leccion04/BD/prueba_bd.py
@@ -1,18 +1,14 @@
 import psycopg2
 
-conexion = psycopg2.connect(
-        user='postgres',
-        password='SuperAdmin',
-        host='localhost',
-        port='5432',
-        database='test'
-)
-
-cursor = conexion.cursor()
-sentencia = "SELECT * FROM persona"
-cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
-registro = cursor.fetchall() # Recuperamos todos los registros que seran una lista
-print(registro)
-
-cursor.close()
-conexion.close()
+conexion = psycopg2.connect(user='postgres', password='SuperAdmin', host='localhost', port='5432', database='test')
+try:
+        with conexion:
+                with conexion.cursor() as cursor:
+                        sentencia = "SELECT * FROM persona"
+                        cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
+                        registro = cursor.fetchall() # Recuperamos todos los registros que seran una lista
+                        print(registro)
+except Exception as e:
+        print(f"Ocurrio un error: {e}")
+finally:
+        conexion.close()
