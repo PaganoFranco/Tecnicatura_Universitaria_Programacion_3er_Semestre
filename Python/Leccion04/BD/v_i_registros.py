520ce261d51a50e15e958c440c67aa257cc6855f
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-29 20:35:28 -0300

    Insertar varios registros

---
+++ Python/Leccion04/BD/v_i_registros.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+conexion = psycopg2.connect(user='postgres', password='SuperAdmin', host='localhost', port='5432', database='test')
+try:
+        with conexion:
+            with conexion.cursor() as cursor:
+                sentencia = "INSERT INTO persona (nombre, apellido, email) VALUES (%s, %s, %s)"
+                valores = (
+                    ("Carlos", "Lopez", "LopezC@gmail.com"),
+                    ("Marcos", "Español", "Españaarriba@gmail.com"),
+                    ("Nelson", "Lassa", "Tuturrito@gmail.com")
+                ) # es una tupla
+                cursor.executemany(sentencia, valores) # De esta manera ejecutamos la sentencia
+                # conexion.commit() esto se utilizar para guardar los cambios en la base de datos
+                registros_insetados = cursor.rowcount
+                print(f"Los registros insertados son: {registros_insetados}")
+
+
+except Exception as e:
+        print(f"Ocurrio un error: {e}")
+finally:
+        conexion.close()
