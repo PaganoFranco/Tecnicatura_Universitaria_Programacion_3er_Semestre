6e3447ba16de3dc42c1895d2d100896c398421eb
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-29 20:24:49 -0300

    Función fechall en psycopg2

--- Python/Leccion04/BD/prueba_bd.py
+++ Python/Leccion04/BD/prueba_bd.py
@@ -7,10 +7,12 @@ try:
                         sentencia = "SELECT * FROM persona WHERE id_persona = %s" # Placeholder
                         id_persona = input("Digite un numero para el id_persona: ")
                         cursor.execute(sentencia, (id_persona, )) # De esta manera ejecutamos la sentencia
-                        registro = cursor.fetchone()  
-                        #registro = cursor.fetchall() Recuperamos todos los registros que seran una lista
+                        registro = cursor.fetchone()
+                        # registro = cursor.fetchall() Recuperamos todos los registros que seran una lista
                         print(registro)
 except Exception as e:
         print(f"Ocurrio un error: {e}")
 finally:
         conexion.close()
+
+# https://www.psycopg.org/docs/usage.html
