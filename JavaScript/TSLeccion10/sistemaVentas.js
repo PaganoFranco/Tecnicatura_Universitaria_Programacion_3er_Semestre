7a559f74418a5d3a24e35a1bd0936d99380c7489
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 20:23:44 -0300

    Pruebas con la relación de Agregación

--- JavaScript/TSLeccion10/sistemaVentas.js
+++ JavaScript/TSLeccion10/sistemaVentas.js
@@ -59,9 +59,34 @@ class Orden {
 
     } // Fin de metodo agregar produucto
 
+    calcularTotal() {
+        let totalVenta = 0;
+        for (let producto of this._productos) {
+            totalVenta += producto.precio;
+        } //Fin ciclo for
+        return totalVenta;
+    } //Fin del metodo calcular
+
+    mostrarOrden() {
+        let productosOrden = ' ';
+        for (let producto of this._productos) {
+            productosOrden += `\n{ ` + producto.toString() + ' }';
+        } //Fin ciclo for
+        console.log(`Orden: ${this._idOrden}, Total : $${this.calcularTotal()}, Productos : ${productosOrden}`);
+    } //Fin del metodo mostrar orden
+
 } //Fin Class Orden
 
 let producto1 = new Producto('Pantalon', 200);
 let producto2 = new Producto('Camisa', 150);
-console.log(producto1.toString());
-console.log(producto2.toString());
+let producto3 = new Producto('Cinturon', 50)
+let orden1 = new Orden();
+let orden2 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.agregarProducto(producto3);
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden1.mostrarOrden();
+orden2.mostrarOrden();
