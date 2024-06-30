e05459f70226e4411b568879feb80747cb3e8bf3
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 11:16:03 -0300

    Cargando todo en una sola plantilla

---
+++ JavaScript/TSLeccion07/PruebaPersonas.js
@@ -0,0 +1,102 @@
+class Persona {
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad) {
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    // Getters
+    get idPersona() {
+        return this._idPersona;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    get apellido() {
+        return this._apellido;
+    }
+
+    get edad() {
+        return this._edad;
+    }
+
+    // Setters
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+
+    set edad(edad) {
+        this._edad = edad;
+    }
+
+    toString(){
+        return this._idPersona+" "+this._nombre+" "+this._apellido+" "+this._edad;
+    }
+}
+
+class Empleado extends Persona {
+    static contadorEmpleados = 0;
+
+    constructor(nombre, apellido, edad, sueldo) {
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._sueldo = sueldo;
+    }
+
+    // Getters
+    get idEmpleado() {
+        return this._idEmpleado;
+    }
+
+    get sueldo() {
+        return this._sueldo;
+    }
+
+    // Setters
+    set sueldo(sueldo) {
+        this._sueldo = sueldo;
+    }
+
+    toString(){
+        return super.toString()+" "+this._idEmpleado+" "+this._sueldo;
+    }
+}
+
+class Cliente extends Persona {
+    static contadorClientes = 0;
+
+    constructor(nombre, apellido, edad, fecharegistro) {
+        super(nombre, apellido, edad);
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fecharegistro = fecharegistro;
+    }
+
+    // Getters
+    get idCliente() {
+        return this._idCliente;
+    }
+
+    get fecharegistro() {
+        return this._fecharegistro;
+    }
+
+    // Setters
+    set fecharegistro(fecharegistro) {
+        this._fecharegistro = fecharegistro;
+    }
+
+    // toString
+    toString() {
+        return super.toString()+" "+ this._idCliente+" "+this.fecharegistro;
+    }
+}
+
