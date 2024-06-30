eeb0835e4bd00b02dddd70baabe8e2c9169528dd
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 18:44:01 -0300

    Cargamos el menú

--- Java/Clase 9 Comenzamos con Proyectos CalculadoraUTN/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Clase 9 Comenzamos con Proyectos CalculadoraUTN/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
-
 public class CalculadoraUTN {
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("****** Aplicacion Calculadora ******");
-        System.out.println("Digite el valor para el operando 1");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.println("Digite el valor para el operando 2");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+        //Mostrar el menu:
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5 . Salir
+                """);
+        System.out.print("¿Operacion a realizar? ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if (operacion >= 1 & operacion <= 4) {
+            System.out.print("Digite el valor para el operando 1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Digite el valor para el operando 2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+            var resultado = operando1 + operando2;
+            System.out.println("resultado = " + resultado);
+        }
     }
 }
