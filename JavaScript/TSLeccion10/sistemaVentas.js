72ad300286c575a92cbd5aec6a4ad8c5dd85a107
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 20:19:10 -0300

    Creamos la Clase Producto -> Usamos Template Literals

---
+++ JavaScript/TSLeccion10/sistemaVentas.js
@@ -0,0 +1,32 @@
+class Producto {
+    static contadorProductos = 0;
+    constructor(nombre, precio) {
+        this._idProducto = ++Producto.contadorProductos;
+        this._nombre = nombre;
+        this._precio = precio;
+    }
+    get idProducto() {
+        return rhis._idProducto;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+    get precio() {
+        return this._precio;
+    }
+
+    set precio(precio) {
+        this._precio;
+    }
+
+    toString() {
+        return `idProducto : ${this._idProducto}, nombre : ${this._nombre}, precio : $${this._precio}`;
+    }
+
+} // Fin Class Producto
