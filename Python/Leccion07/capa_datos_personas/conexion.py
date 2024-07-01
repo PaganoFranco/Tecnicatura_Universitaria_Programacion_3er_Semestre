260e7d01da16a7e17b16267adad9c926c8503535
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 23:49:36 -0300

    POOL DE CONEXIONES CON PYTHON Y POSTGRESQL

--- Python/Leccion07/capa_datos_personas/conexion.py
+++ Python/Leccion07/capa_datos_personas/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 import sys
 
@@ -9,40 +9,23 @@ class Conexion:
     _PASSWORD = "SuperAdmin"
     _DB_PORT = "5432"
     _HOST = "127.0.0.1"
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
-                log.debug(f"Conexión exitosa: {cls._conexion}")
-                return cls._conexion
-            except Exception as e:
-                log.error(f"Ocurrió un error: {e}")
-                sys.exit()
-        else:
-            return cls._conexion
+        pass
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f"Se abrió correctamente el cursor: {cls._cursor}")
-                return cls._cursor
-            except Exception as e:
-                log.error(f"Ocurrió un error: {e}")
-                sys.exit()
-        else:
-            return cls._cursor
+        pass
 
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool()
 
 if __name__ == "__main__":
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass
