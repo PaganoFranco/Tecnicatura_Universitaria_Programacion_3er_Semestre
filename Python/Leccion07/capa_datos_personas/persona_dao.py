3bcc3aad98832189ccfa03775821b6f86d144fcf
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-06-30 23:31:09 -0300

    Método Actualizar

--- Python/Leccion07/capa_datos_personas/persona_dao.py
+++ Python/Leccion07/capa_datos_personas/persona_dao.py
@@ -41,11 +41,27 @@ class PersonaDAO:
                 log.debug(f"Persona Insertada: {persona}")
                 return cursor.rowcount
 
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f"Persona actualizada: {persona}")
+                return cursor.rowcount
+
+
 if __name__ == "__main__":
+    # Actualizar un registro
+    persona1 = Persona(1, "Franco", "Pagano", "FrancoP@gmail.com")
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f"Personas actualizadas: {personas_actualizadas}")
+
     # Insertar un registro
-    persona1 = Persona(nombre="Franco", apellido="Pagano", email="FrancoP@gmail.com")
-    personas_insertadas = PersonaDAO.insertar(persona1)
-    log.debug(f"Personas insertadas: {personas_insertadas}")
+    # persona1 = Persona(nombre="Juan", apellido="Perez", email="JuanP@gmail.com")
+    # personas_insertadas = PersonaDAO.insertar(persona1)
+    # log.debug(f"Personas insertadas: {personas_insertadas}")
+
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
